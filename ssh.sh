37dd6ab0ef1cbc6be3c4823faf42f25e626c3ee9
marzban2030 <73051714+marzban2030@users.noreply.github.com>
2023-05-27 20:52:28 +0330

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Remote port forwarding by ssh command in bash script environment 
-/mnt/sdcard/ld-linux.so.3 --library-path /mnt/sdcard /mnt/sdcard/ssh -N -f -tt -R 23:127.0.0.1:23 -R 21:127.0.0.1:21 -i /mnt/mtd/id_rsa -o UserKnownHostsFile=/mnt/mtd/known_hosts -o StrictHostKeyChecking=no root@REMOTE_HOST_ADDRESS -p 22
+/mnt/sdcard/ld-linux.so.3 --library-path /mnt/sdcard /mnt/sdcard/ssh -N -f -tt -R 0.0.0.0:23:127.0.0.1:23 -R 21:127.0.0.1:21 -i /mnt/mtd/id_rsa -o UserKnownHostsFile=/mnt/mtd/known_hosts -o StrictHostKeyChecking=no root@REMOTE_HOST_ADDRESS -p 22
